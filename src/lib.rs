d62a79f5410742bdfa0b781d8f7cdd4ca88ae5cd
Francois Gindraud <francois.gindraud@inria.fr>
2024-01-15 19:13:11 +0100

    Metadata API improvements
    Decouple the offset computation from field access and DST synthesis.
    Now the Field trait does not care about offset.
    It can take self in methods which feels more ergonomic.
    
    deref/deref_mut/drop_in_place from base pointer are now defined as
    private methods of Metadata : less internal details exposed.
    drop_in_place is always defined as drop_in_place(deref_mut()), until we
    find a need to specialize it.
    
    For consistency, Initializer trait also leaves offset computation to
    Metadata methods.
    Added initialize for InitializerSequence too.
    
    Some renaming of methods to differentiate between scope of operation :
    - initialize in the trait, actual operation on field_addr
    - Metadata::initialize_field where it handles offset computation
    - InitializerSeauence::initialize_fields for tuple unpacking

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,7 @@ impl<H, FS: FieldSequence> Box<H, FS> {
 ///////////////////////////////////////////////////////////////////////////////
 
 /// Represent a sequence of concatenated fields
-pub trait FieldSequence {
+pub unsafe trait FieldSequence {
     /// Compound metadata for the sequence (tuple of metadata usually).
     type Metadata;
 
@@ -42,79 +42,89 @@ pub trait FieldSequence {
     unsafe fn drop_in_place(metadata: &mut Self::Metadata, base: *mut u8);
 }
 
-pub trait InitializerSequence<Fields>
-where
-    Fields: FieldSequence,
-{
-    fn extend_with_metadata(
+/// Represent a sequence of initializer that can initialize a [`FieldSequence`]
+pub unsafe trait InitializerSequence<Fields: FieldSequence> {
+    fn extend_layout_with_fields(
         &self,
         layout: &Layout,
     ) -> Result<(Layout, Fields::Metadata), LayoutError>;
+
+    unsafe fn initialize_fields(self, base: *mut u8, metadata: &Fields::Metadata);
 }
 
 // T
-impl<F: Field> FieldSequence for F {
+unsafe impl<F: Field> FieldSequence for F {
     type Metadata = Metadata<F>;
     type Ref<'m> = &'m F::Target where Self: 'm;
     type RefMut<'m> = &'m mut F::Target where Self: 'm;
 
     unsafe fn deref(metadata: &Self::Metadata, base: *const u8) -> Self::Ref<'_> {
-        Field::deref(metadata, base)
+        metadata.deref(base)
     }
     unsafe fn deref_mut(metadata: &mut Self::Metadata, base: *mut u8) -> Self::RefMut<'_> {
-        Field::deref_mut(metadata, base)
+        metadata.deref_mut(base)
     }
     unsafe fn drop_in_place(metadata: &mut Self::Metadata, base: *mut u8) {
-        Field::drop_in_place(metadata, base)
+        metadata.drop_in_place(base)
     }
 }
-impl<F, T> InitializerSequence<F> for T
+unsafe impl<F, T> InitializerSequence<F> for T
 where
     F: Field,
     T: Initializer<F>,
 {
-    fn extend_with_metadata(
+    fn extend_layout_with_fields(
         &self,
         layout: &Layout,
     ) -> Result<(Layout, <F as FieldSequence>::Metadata), LayoutError> {
-        extend_layout_with_metadata(self, layout)
+        extend_layout_with_field(self, layout)
+    }
+
+    unsafe fn initialize_fields(self, base: *mut u8, metadata: &<F as FieldSequence>::Metadata) {
+        metadata.initialize_field(base, self)
     }
 }
 
 // (T0, T1)
-impl<F0: Field, F1: Field> FieldSequence for (F0, F1) {
+unsafe impl<F0: Field, F1: Field> FieldSequence for (F0, F1) {
     type Metadata = (Metadata<F0>, Metadata<F1>);
     type Ref<'m> = (&'m F0::Target, &'m F1::Target) where Self: 'm;
     type RefMut<'m> = (&'m mut F0::Target, &'m mut F1::Target) where Self: 'm;
 
     unsafe fn deref(metadata: &Self::Metadata, base: *const u8) -> Self::Ref<'_> {
-        (F0::deref(&metadata.0, base), F1::deref(&metadata.1, base))
+        (metadata.0.deref(base), metadata.1.deref(base))
     }
     unsafe fn deref_mut(metadata: &mut Self::Metadata, base: *mut u8) -> Self::RefMut<'_> {
-        (
-            F0::deref_mut(&mut metadata.0, base),
-            F1::deref_mut(&mut metadata.1, base),
-        )
+        (metadata.0.deref_mut(base), metadata.1.deref_mut(base))
     }
     unsafe fn drop_in_place(metadata: &mut Self::Metadata, base: *mut u8) {
-        Field::drop_in_place(&mut metadata.0, base);
-        Field::drop_in_place(&mut metadata.1, base);
+        metadata.0.drop_in_place(base);
+        metadata.1.drop_in_place(base);
     }
 }
-impl<F0, F1, T0, T1> InitializerSequence<(F0, F1)> for (T0, T1)
+unsafe impl<F0, F1, T0, T1> InitializerSequence<(F0, F1)> for (T0, T1)
 where
     F0: Field,
     F1: Field,
     T0: Initializer<F0>,
     T1: Initializer<F1>,
 {
-    fn extend_with_metadata(
+    fn extend_layout_with_fields(
         &self,
         layout: &Layout,
     ) -> Result<(Layout, <(F0, F1) as FieldSequence>::Metadata), LayoutError> {
-        let (layout, metadata0) = extend_layout_with_metadata(&self.0, layout)?;
-        let (layout, metadata1) = extend_layout_with_metadata(&self.1, &layout)?;
+        let (layout, metadata0) = extend_layout_with_field(&self.0, layout)?;
+        let (layout, metadata1) = extend_layout_with_field(&self.1, &layout)?;
         Ok((layout, (metadata0, metadata1)))
+    }
+
+    unsafe fn initialize_fields(
+        self,
+        base: *mut u8,
+        metadata: &<(F0, F1) as FieldSequence>::Metadata,
+    ) {
+        metadata.0.initialize_field(base, self.0);
+        metadata.1.initialize_field(base, self.1)
     }
 }
 
@@ -124,6 +134,7 @@ where
 ///
 /// [`Self`] in this case is the field specific metadata : slice length, `dyn` vtable.
 /// It is created from various initializers defined by the [`Initializer`] trait.
+/// This metadata is packed with a memory offset in a [`Metadata`] struct to be able to access the field from the base allocation.
 ///
 /// This trait is unsafe to implement, as packed DST containers will use its functions assuming invariants are met.
 /// The functions themselves are unsafe due to creating reference to the DST field from raw pointers.
@@ -131,31 +142,11 @@ pub unsafe trait Field {
     /// Type stored in the field, can be a DST : `[T]`, `dyn Trait`.
     type Target: ?Sized;
 
-    // Derefs take the metadata by reference to tie the lifetimes of the header to the DST lifetime.
-    // Safety: base points to start of valid packed allocation to which metadata is attached, and field referenced by metadata has been initialized.
-    unsafe fn deref(metadata: &Metadata<Self>, base: *const u8) -> &Self::Target;
-    unsafe fn deref_mut(metadata: &mut Metadata<Self>, base: *mut u8) -> &mut Self::Target;
-    unsafe fn drop_in_place(metadata: &mut Metadata<Self>, base: *mut u8);
-
-    // TODO add *_first variants with layout offset computation ?
-    // FIXME is drop_in_place necessary as it is always drop_in_place(deref_mut()) ?
-}
-
-/// Complete metadata for a [`Field`] : ties the field metadata with its offset.
-pub struct Metadata<F: ?Sized> {
-    /// Offset of field from base of allocation, in bytes.
-    offset: usize,
-    /// Field specific metadata.
-    field: F,
-}
-impl<F: ?Sized> Metadata<F> {
-    /// Offset from base of allocation in bytes.
+    /// Derefs take the metadata by reference to tie the lifetimes of the header to the DST lifetime.
     ///
-    /// SAFETY: must be a valid metadata attached to a valid packed allocation.
-    unsafe fn offset(&self) -> isize {
-        // Alloc cannot allocate more than isize::MAX
-        isize::try_from(self.offset).unwrap_unchecked()
-    }
+    /// SAFETY: field_addr must point to a valid initialized field that the metadata represents.
+    unsafe fn deref(&self, field_addr: *const u8) -> &Self::Target;
+    unsafe fn deref_mut(&mut self, field_addr: *mut u8) -> &mut Self::Target;
 }
 
 /// Defines what can initialize a [`Field`].
@@ -165,20 +156,64 @@ impl<F: ?Sized> Metadata<F> {
 /// - using the values to initialize the fresh allocation at the field offset.
 pub unsafe trait Initializer<Field> {
     /// Computes the required layout and metadata for the field.
-    fn metadata(&self) -> Result<(Layout, Field), LayoutError>;
+    fn analyze(&self) -> Result<(Layout, Field), LayoutError>;
 
     /// Initialize the allocation memory with the content of the initializer.
     ///
-    /// `field_addr` is guaranteed to match the layout requirements of [`Self::metadata`].
-    /// It must be initialized by this method.
-    unsafe fn initialize_memory(self, field_addr: *mut u8);
+    /// SAFETY: `field_addr` must match the layout requirements of [`Self::analyze`].
+    unsafe fn initialize(self, field_addr: *mut u8);
 }
 
-fn extend_layout_with_metadata<F, T: Initializer<F>>(
+/// Complete metadata for a [`Field`] : ties the field metadata with its offset.
+///
+/// TODO add a First variant with recomputed offset ?
+pub struct Metadata<F> {
+    /// Offset of field from base of allocation, in bytes.
+    offset: usize,
+    /// Field specific metadata.
+    field: F,
+}
+
+impl<F> Metadata<F> {
+    /// Offset from base of allocation in bytes.
+    ///
+    /// SAFETY: must be a valid metadata attached to a valid packed allocation.
+    unsafe fn offset(&self) -> isize {
+        // Alloc cannot allocate more than isize::MAX
+        isize::try_from(self.offset).unwrap_unchecked()
+    }
+
+    // SAFETY: base must have the right layout at offset for initialization.
+    unsafe fn initialize_field<T: Initializer<F>>(&self, base: *mut u8, initializer: T) {
+        let field_addr = base.offset(self.offset());
+        initializer.initialize(field_addr)
+    }
+}
+
+/// [`Field`] operations from *base* allocation pointer ; applies offset.
+///
+/// SAFETY: must be used on a pointer to valid initialized memory which matches the layout and offset of self.
+impl<F: Field> Metadata<F> {
+    unsafe fn deref(&self, base: *const u8) -> &F::Target {
+        let field_addr = base.offset(self.offset());
+        F::deref(&self.field, field_addr)
+    }
+    unsafe fn deref_mut(&mut self, base: *mut u8) -> &mut F::Target {
+        let field_addr = base.offset(self.offset());
+        F::deref_mut(&mut self.field, field_addr)
+    }
+    unsafe fn drop_in_place(&mut self, base: *mut u8) {
+        // For all covered DST ([T], str, dyn trait) we can use drop_in_place(*mut DST).
+        // It can be added to the Field trait in case of specialized behavior.
+        std::ptr::drop_in_place(self.deref_mut(base));
+    }
+}
+
+fn extend_layout_with_field<F, T: Initializer<F>>(
     initializer: &T,
     layout: &Layout,
 ) -> Result<(Layout, Metadata<F>), LayoutError> {
-    let (field_layout, field) = initializer.metadata()?;
+    let (field_layout, field) = initializer.analyze()?;
     let (layout, offset) = layout.extend(field_layout)?;
     let metadata = Metadata { offset, field };
     Ok((layout, metadata))
@@ -200,15 +235,11 @@ pub struct Slice<T> {
 unsafe impl<T> Field for Slice<T> {
     type Target = [T];
 
-    unsafe fn deref(metadata: &Metadata<Self>, base: *const u8) -> &Self::Target {
-        std::slice::from_raw_parts(base.offset(metadata.offset()).cast(), metadata.field.length)
+    unsafe fn deref(&self, field_addr: *const u8) -> &Self::Target {
+        std::slice::from_raw_parts(field_addr.cast(), self.length)
     }
-    unsafe fn deref_mut(metadata: &mut Metadata<Self>, base: *mut u8) -> &mut Self::Target {
-        std::slice::from_raw_parts_mut(base.offset(metadata.offset()).cast(), metadata.field.length)
-    }
-    unsafe fn drop_in_place(metadata: &mut Metadata<Self>, base: *mut u8) {
-        // std::vec::Vec uses drop_in_place on the slice
-        std::ptr::drop_in_place(Field::deref_mut(metadata, base));
+    unsafe fn deref_mut(&mut self, field_addr: *mut u8) -> &mut Self::Target {
+        std::slice::from_raw_parts_mut(field_addr.cast(), self.length)
     }
 }
 
@@ -216,7 +247,7 @@ unsafe impl<T, Iter> Initializer<Slice<T>> for Iter
 where
     Iter: std::iter::TrustedLen<Item = T>,
 {
-    fn metadata(&self) -> Result<(Layout, Slice<T>), LayoutError> {
+    fn analyze(&self) -> Result<(Layout, Slice<T>), LayoutError> {
         // Final allocation is never 0 due to metadata, so no special case.
         // If the iterator exceeds isize::MAX the final allocation would fail so no need for a size check.
         let (lower, _higher) = self.size_hint();
@@ -228,7 +259,7 @@ where
         };
         Ok((layout, metadata))
     }
-    unsafe fn initialize_memory(self, field_addr: *mut u8) {
+    unsafe fn initialize(self, field_addr: *mut u8) {
         let mut ptr: *mut T = field_addr.cast();
         for v in self {
             ptr.write(v);
@@ -239,7 +270,7 @@ where
 
 ///////////////////////////////////////////////////////////////////////////////
 
-/// Represent a field containing a [`String`] data.
+/// Represent a field containing a [`String`].
 ///
 /// Must be initialized by copying data from a [`&str`].
 pub struct Str {
@@ -250,23 +281,18 @@ pub struct Str {
 unsafe impl Field for Str {
     type Target = str;
 
-    unsafe fn deref(metadata: &Metadata<Self>, base: *const u8) -> &Self::Target {
-        let s = std::slice::from_raw_parts(base.offset(metadata.offset()), metadata.field.length);
+    unsafe fn deref(&self, field_addr: *const u8) -> &Self::Target {
+        let s = std::slice::from_raw_parts(field_addr, self.length);
         std::str::from_utf8_unchecked(s)
     }
-    unsafe fn deref_mut(metadata: &mut Metadata<Self>, base: *mut u8) -> &mut Self::Target {
-        let s =
-            std::slice::from_raw_parts_mut(base.offset(metadata.offset()), metadata.field.length);
+    unsafe fn deref_mut(&mut self, field_addr: *mut u8) -> &mut Self::Target {
+        let s = std::slice::from_raw_parts_mut(field_addr, self.length);
         std::str::from_utf8_unchecked_mut(s)
-    }
-    unsafe fn drop_in_place(metadata: &mut Metadata<Self>, base: *mut u8) {
-        // Use drop_in_place from str, should do nothing
-        std::ptr::drop_in_place(Field::deref_mut(metadata, base))
     }
 }
 
 unsafe impl Initializer<Str> for &'_ str {
-    fn metadata(&self) -> Result<(Layout, Str), LayoutError> {
+    fn analyze(&self) -> Result<(Layout, Str), LayoutError> {
         let length = self.len();
         let layout = Layout::array::<u8>(length)?;
         let metadata = Str {
@@ -275,7 +301,7 @@ unsafe impl Initializer<Str> for &'_ str {
         };
         Ok((layout, metadata))
     }
-    unsafe fn initialize_memory(self, field_addr: *mut u8) {
+    unsafe fn initialize(self, field_addr: *mut u8) {
         std::ptr::copy_nonoverlapping(self.as_ptr(), field_addr, self.len())
     }
 }
@@ -298,19 +324,13 @@ pub struct Unsized<Dyn> {
 unsafe impl<Dyn> Field for Unsized<Dyn> {
     type Target = Dyn;
 
-    unsafe fn deref(metadata: &Metadata<Self>, base: *const u8) -> &Self::Target {
-        let value_addr = base.offset(metadata.offset());
-        let ptr = std::ptr::from_raw_parts(value_addr.cast(), metadata.field.metadata);
+    unsafe fn deref(&self, field_addr: *const u8) -> &Self::Target {
+        let ptr = std::ptr::from_raw_parts(field_addr.cast(), self.metadata);
         &*ptr
     }
-    unsafe fn deref_mut(metadata: &mut Metadata<Self>, base: *mut u8) -> &mut Self::Target {
-        let value_addr = base.offset(metadata.offset());
-        let ptr = std::ptr::from_raw_parts_mut(value_addr.cast(), metadata.field.metadata);
+    unsafe fn deref_mut(&mut self, field_addr: *mut u8) -> &mut Self::Target {
+        let ptr = std::ptr::from_raw_parts_mut(field_addr.cast(), self.metadata);
         &mut *ptr
-    }
-    unsafe fn drop_in_place(metadata: &mut Metadata<Self>, base: *mut u8) {
-        // Delegate to drop for Dyn. Will call the vtable if necessary.
-        std::ptr::drop_in_place(Field::deref_mut(metadata, base))
     }
 }
 
@@ -318,7 +338,7 @@ unsafe impl<Dyn, T> Initializer<Unsized<Dyn>> for T
 where
     T: std::marker::Unsize<Dyn>,
 {
-    fn metadata(&self) -> Result<(Layout, Unsized<Dyn>), LayoutError> {
+    fn analyze(&self) -> Result<(Layout, Unsized<Dyn>), LayoutError> {
         let layout = Layout::for_value(self);
         let metadata = std::ptr::metadata(self as &Dyn);
         let metadata = Unsized {
@@ -327,7 +347,7 @@ where
         };
         Ok((layout, metadata))
     }
-    unsafe fn initialize_memory(self, field_addr: *mut u8) {
+    unsafe fn initialize(self, field_addr: *mut u8) {
         let ptr: *mut T = field_addr.cast();
         ptr.write(self)
     }
